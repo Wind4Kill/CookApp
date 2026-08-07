8f25567ec7213ba115c0e35708dee48d943bff42
Artem <artemrom1337@gmail.com>
2026-08-07 18:25:39 +0300

    FiltrationFix

--- CookApp.Model/DTOs/FiltrationDTO.cs
+++ CookApp.Model/DTOs/FiltrationDTO.cs
@@ -13,9 +13,9 @@ namespace CookApp.Model.DTOs
     {
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
-            if (Enum.TryParse<FiltrationFilter>(FiltrationType, out FiltrationFilter result))
+            if (FiltrationType is not null&&FiltrationType!=FiltrationFilter.Default.ToString())
             {
-                if (result != FiltrationFilter.Default && FiltrationData is null)
+                if (FiltrationData is null)
                 {
                     yield return new ValidationResult("Filtration type other from default must have filtration value.", [nameof(FiltrationData)]);
                 }
